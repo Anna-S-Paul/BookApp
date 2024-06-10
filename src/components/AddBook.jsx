ab3033e027a5d48c11d96a8807d213062b72ed15
AnnaJ <annaspaulimp@gmail.com>
2024-06-10 22:51:55 +0530

    successful

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -4,11 +4,11 @@ import NavBook from './NavBook'
 const AddBook = () => {
     return (
         <div>
-<NavBook/>
+            <NavBook />
             <div className="container">
-                <div className="row g-3">
+                <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <div className="row">
+                        <div className="row g-3">
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
 
                                 <label htmlFor="" className="form-label">Book title:</label>
@@ -19,23 +19,12 @@ const AddBook = () => {
 
                                 <label htmlFor="" className="form-label">Category:</label>
                                 <select name="" id="" className="form-control">
+                                    <option value="Sci-Fi">--select--</option>
                                     <option value="Sci-Fi">Sci-Fi</option>
                                     <option value="Fantasy">Fantasy</option>
                                     <option value="Romance">Romance</option>
                                     <option value="Poetry">Poetry</option>
                                 </select>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
-
-                                <label htmlFor="" className="form-label">Published year:</label>
-                                <input type="date" name="" id="" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
-
-                                <label htmlFor="" className="form-label">Author name:</label>
-                                <input type="text" className="form-control" />
 
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -46,13 +35,7 @@ const AddBook = () => {
                             </div>
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
 
-                                <label htmlFor="" className="form-label">Distributor name:</label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
-
-                                <label htmlFor="" className="form-label">Publisher name:</label>
+                                <label htmlFor="" className="form-label">Author name:</label>
                                 <input type="text" className="form-control" />
 
                             </div>
@@ -60,12 +43,6 @@ const AddBook = () => {
 
                                 <label htmlFor="" className="form-label">Price:</label>
                                 <input type="number" name="" id="" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
-
-                                <label htmlFor="" className="form-label">Edition:</label>
-                                <input type="text" className="form-control" />
 
                             </div>
                             <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3 col-xxl-3">
--- src/components/ViewAllBooks.jsx
+++ src/components/ViewAllBooks.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBook from './NavBook'
 
 const ViewAllBooks = () => {
+  const [data,setData]=useState(
+    [ 
+      {"title":"HARRY POTTER","category":"fantasy","description":"Avery magical book","authorname":"POTTER","price":1500},
+      {"title":"PHYSICS","category":"Sci-Fi","description":"Avery Physical book","authorname":"NEUTON","price":600},
+      {"title":"LORD OF RINGS","category":"Sci-Fi","description":"Avery incredible book","authorname":"Kinger","price":1500}
+    ]
+  )
   return (
     <div>
 <NavBook/>
@@ -13,60 +20,26 @@ const ViewAllBooks = () => {
   <thead>
     <tr>
       <th scope="col">Book title</th>
+      <th scope="col">Category</th>
       <th scope="col">Author name</th>
-      <th scope="col">Published year</th>
+      <th scope="col">Description</th>
       <th scope="col">Price</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>GODFATHER</td>
-      <td>Newton</td>
-      <td>2002</td>
-      <td>1000/-</td>
-    </tr>
-    <tr>
-      <td>SHEDULER</td>
-      <td>Abdul</td>
-      <td>2000</td>
-      <td>1500/-</td>
-    </tr>
-    <tr>
-      <td>FATHER AND SON</td>
-      <td>Charles</td>
-      <td>1989</td>
-      <td>1000/-</td>
-    </tr>
-    <tr>
-      <td>ASUS</td>
-      <td>Tim</td>
-      <td>2012</td>
-      <td>1500/-</td>
-    </tr>
-    <tr>
-      <td>HP</td>
-      <td>Anna</td>
-      <td>2024</td>
-      <td>3000/-</td>
-    </tr>
-    <tr>
-      <td>GUJJU</td>
-      <td>Sagar</td>
-      <td>1999</td>
-      <td>500/-</td>
-    </tr>
-    <tr>
-      <td>GANDHI</td>
-      <td>Nehru</td>
-      <td>1918</td>
-      <td>200/-</td>
-    </tr>
-    <tr>
-      <td>KERNEL</td>
-      <td>Newton</td>
-      <td>1967</td>
-      <td>1500/-</td>
-    </tr>
+    {
+      data.map(
+        (value,index)=>{
+          return <tr>
+          <td>{value.title}</td>
+          <td>{value.category}</td>
+          <td>{value.description}</td>
+          <td>{value.authorname}</td>
+          <td>{value.price}</td>
+        </tr>
+        }
+      )
+    }
   </tbody>
 </table>
 
